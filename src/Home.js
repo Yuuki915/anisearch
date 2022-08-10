6b4901b170b5aba53f67981f2f9200679cece012
Yuuki915 <aso091335@gmail.com>
2022-08-10 02:21:35 -0700

    display changed

--- src/Home.js
+++ src/Home.js
@@ -1,51 +1,39 @@
 import React from "react";
 
-import { gql, useQuery, useMutation } from "@apollo/client";
-// import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import Footer from "./components/partials/footer/Footer";
 import Header from "./components/partials/header/Header";
 import Main from "./components/pages/top/main/Main";
 import Sidebar from "./components/pages/top/sidebar/Sidebar";
-
 import { GET_ALL_ANIME } from "./components/GetAnimes";
+import {
+  Data16,
+  Data17,
+  Data18,
+  Data19,
+  Data20,
+  Data21,
+  Data22,
+} from "./components/EachYear";
 
 export default function Home() {
-  // const GET_TODOS = gql`
-  //   query getTodos {
-  //     todos {
-  //       done
-  //       id
-  //       text
-  //     }
-  //   }
-  // `;
-
-  const ADD_TODO = gql`
-    mutation addTodo($text: String!) {
-      insert_todos(objects: { text: $text }) {
-        returning {
-          done
-          id
-          text
-        }
-      }
-    }
-  `;
   const { data, loading, error } = useQuery(GET_ALL_ANIME);
+  const animeData = data && data.searchWorks.edges;
+  const animeTv =
+    animeData && animeData.filter((item) => item.node.media === "TV");
+  const imgNull = animeTv && animeTv.filter((item) => item.node.image === null);
+  const dataAll = animeTv && animeTv.filter((item) => item.node.image !== null);
 
-  // const [todoText, setTodoText] = useState("");
-  // const [addTodo] = useMutation(ADD_TODO);
+  const data22 = Data22();
+  const data21 = Data21();
+  const data20 = Data20();
+  const data19 = Data19();
+  const data18 = Data18();
+  const data17 = Data17();
+  const data16 = Data16();
 
-  // const handleAddTodo = async (e) => {
-  //   e.preventDefault();
-
-  //   if (!todoText.trim()) return;
-  //   const data = await addTodo({
-  //     variables: { text: todoText },
-  //     refetchQueries: [{ query: GET_ANIME_2022 }],
-  //   });
-  // };
-
+  console.log(dataAll);
+  
   if (loading)
     return (
       <>
@@ -65,19 +53,31 @@ export default function Home() {
       </>
     );
 
-  const animeObj = data.searchWorks.edges;
-  const tvAnime =
-    animeObj && animeObj.filter((item) => item.node.media === "TV");
-  const imgNull = tvAnime && tvAnime.filter((item) => item.node.image === null);
-  const imgOk = tvAnime && tvAnime.filter((item) => item.node.image !== null);
-
   return (
     <div className="home">
-      <Header data={imgOk} />
+      <Header
+        dataAll={dataAll}
+        data22={data22}
+        data21={data21}
+        data20={data20}
+        data19={data19}
+        data18={data18}
+        data17={data17}
+        data16={data16}
+      />
 
       <div className="main-container">
         <Sidebar />
-        <Main />
+        <Main
+          dataAll={dataAll}
+          data22={data22}
+          data21={data21}
+          data20={data20}
+          data19={data19}
+          data18={data18}
+          data17={data17}
+          data16={data16}
+        />
       </div>
 
       <Footer />
--- src/components/pages/top/main/Category.js
+++ src/components/pages/top/main/Category.js
@@ -1,18 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Cards from "../../../partials/cards/Cards";
 
-export default function Category(props) {
-  const imgOk = props.imgOk;
-
+export default function Category({ showData, year }) {
   return (
     <div className="category">
       <div className="header-more">
-        <h2>{imgOk[0].node.seasonYear}'s TV Anime</h2>
-        <p className="read-more">Chack more</p>
+        <h2>{year === 9999 ? "All TV Anime" : `${year}'s TV Anime`}</h2>
+
+        {/* <Link
+          to={`/category/${showData[0] && showData[0].node.seasonYear}`}
+          className="read-more"
+        >
+          Chack more
+        </Link> */}
       </div>
 
       <div className="cards-container">
-        <Cards imgOk={props.imgOk} />
+        <Cards imgOk={showData} />
       </div>
     </div>
   );
