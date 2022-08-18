285451e6d68fe6d25c56e7a37befccb680ea7b7b
Yuuki915 <aso091335@gmail.com>
2022-08-17 22:29:15 -0700

    modify search

--- src/components/partials/cards/Cards.js
+++ src/components/partials/cards/Cards.js
@@ -2,21 +2,20 @@ import "./Cards.css";
 import React from "react";
 import Card from "./Card";
 
-export default function Cards(props) {
-  const imgOk = props.imgOk;
-
+export default function Cards({ imgOk }) {
   return (
     <>
       <div className="cards">
-        {imgOk.slice(0, 7).map((data) => (
-          <Card
-            key={data.node.id}
-            titleEn={data.node.titleEn}
-            titleJp={data.node.title}
-            episodes={data.node.episodesCount}
-            img={data.node.image.recommendedImageUrl}
-          />
-        ))}
+        {imgOk &&
+          imgOk.map((data) => (
+            <Card
+              key={data.node.id}
+              titleEn={data.node.titleEn}
+              titleJp={data.node.title}
+              episodes={data.node.episodesCount}
+              img={data.node.image.recommendedImageUrl}
+            />
+          ))}
       </div>
     </>
   );
--- src/components/partials/header/Header.js
+++ src/components/partials/header/Header.js
@@ -1,66 +1,8 @@
 import "./Header.css";
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-import { CgSearch, CgClose } from "react-icons/cg";
-
-export default function Header(props) {
-  const data = props.data;
-
-  const [count, setCount] = useState(1);
-  const [searchFormToggle, setSearchFormToggle] = useState(false);
-  const [togglePlaceholder, setTogglePlaceholder] = useState("ABC");
-  const [searchedData, setSearchedData] = useState([]);
-  const [inputValue, setInputValue] = useState("");
-
-  const clickToggleHandler = () => {
-    setCount(count + 1);
-    if (count % 2 === 0) {
-      setSearchFormToggle(false);
-      setTogglePlaceholder("ABC");
-    } else {
-      setSearchFormToggle(!false);
-      setTogglePlaceholder("あいう");
-    }
-  };
-
-  const clearInput = () => {
-    setSearchedData([]);
-    setInputValue("");
-  };
-
-  const handleFilter = (e) => {
-    const searchedValue = e.target.value;
-    setInputValue(searchedValue);
-
-    let searchedEn;
-    let searchedJp;
-    if (searchFormToggle === false) {
-      searchedEn = data.filter((val) => {
-        return val.node.titleEn
-          .toLowerCase()
-          .includes(searchedValue.toLowerCase());
-      });
-      if (searchedValue === "") {
-        setSearchedData([]);
-      } else {
-        setSearchedData(searchedEn);
-      }
-    } else {
-      searchedJp = data.filter((val) => {
-        return val.node.titleKana
-          .toLowerCase()
-          .includes(searchedValue.toLowerCase());
-      });
-
-      if (searchedValue === "") {
-        setSearchedData([]);
-      } else {
-        setSearchedData(searchedJp);
-      }
-    }
-  };
-
+export default function Header() {
   return (
     <div className="header">
       <h1>AniSearch</h1>
@@ -79,60 +21,6 @@ export default function Header(props) {
           Home
         </a>
       </nav>
-
-      {/*   ↓↓ Search form ↓↓   */}
-
-      <div className="search-form">
-        <div className="search-inputs">
-          <div className="search-lang">
-            <div className="lang-change" onClick={clickToggleHandler}>
-              <span className={searchFormToggle ? "search-wait" : "search-en"}>
-                A
-              </span>
-              <span>/</span>
-              <span className={searchFormToggle ? "search-jp" : "search-wait"}>
-                あ
-              </span>
-            </div>
-          </div>
-          <input
-            type="text"
-            placeholder={`Search ${togglePlaceholder}`}
-            className="search-input"
-            value={inputValue}
-            onChange={handleFilter}
-          />
-          <div className="search-icon">
-            {searchedData.length === 0 ? (
-              <CgSearch />
-            ) : (
-              <CgClose onClick={clearInput} className="clear-btn" />
-            )}
-          </div>
-        </div>
-
-        {inputValue.length >= 2 ? (
-          searchedData.length !== 0 && (
-            <div className="data-lists">
-              {searchedData.slice(0, 10).map((item) => {
-                return (
-                  <a
-                    key={item.node.id}
-                    className="data-item"
-                    href={item.node.titleEn}
-                  >
-                    <p className="data-title">
-                      {searchFormToggle ? item.node.title : item.node.titleEn}
-                    </p>
-                  </a>
-                );
-              })}
-            </div>
-          )
-        ) : (
-          <></>
-        )}
-      </div>
     </div>
   );
 }
